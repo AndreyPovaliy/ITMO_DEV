8900f95692ca545dd553263d5eb56e2caf185b23
AndreyPovaliy <povandrey16@yandex.ru>
2025-11-04 19:23:44 +0300

    update data

--- 10_Python/lessons/git_work.bash
+++ 10_Python/lessons/git_work.bash
@@ -1,7 +1,25 @@
+Вначале создать профиль на GitHub: -> нужен URL
+
+…or create a NEW repository on the command line
+echo "# NAME" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/YOUR_REPO_URL
+git push -u origin main
+
+
+…or push an EXISTING repository from the command line
+git remote add origin https://github.com/YOUR_REPO_URL
+git branch -M main
+git push -u origin main
+
+
 # узнать статус изменений актуальный
 git status
 # добавить статус в видимую зону
-git add FILENAME (or -A)
+git add FILENAME (or -A - это все) # тут добавить то что хотим зафиксировать
 # зафиксировать изменений с описанием
 git commit -m "TYPE DESCRIPTION"
 # получить журнал записи
